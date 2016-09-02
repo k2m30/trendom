7ed0825056cdc51b027464238bf6098b79bc02b5
Mikhail Chuprynski <mikhail.chuprynski@gmail.com>
2016-09-02 12:47:18 +0300

    not found 404 pages handler

--- app/controllers/not_found_controller.rb
+++ app/controllers/not_found_controller.rb
@@ -1,6 +1,6 @@
 class NotFoundController < ApplicationController
   def any
     Logger.new('log/not_found.log').info(request.fullpath)
-    head :not_found
+    render file: 'public/404.html', status: :not_found, layout: false
   end
 end
