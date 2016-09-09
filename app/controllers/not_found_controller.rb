aedaab1812a0317bc2fd818a39f25bcbf28f4034
Mikhail Chuprynski <mikhail.chuprynski@gmail.com>
2016-09-09 14:41:18 +0300

    skip csrf for not_found

--- app/controllers/not_found_controller.rb
+++ app/controllers/not_found_controller.rb
@@ -1,4 +1,5 @@
 class NotFoundController < ApplicationController
+  skip_before_action :verify_authenticity_token
   def any
     logger.not_found(request.fullpath)
     render file: Rails.root.join('public', '404.html'), status: :not_found, layout: false
