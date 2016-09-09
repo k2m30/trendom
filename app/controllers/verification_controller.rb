d07061f5643584c11ce5d0479e91e88deba0c644
Mikhail Chuprynski <mikhail.chuprynski@gmail.com>
2016-09-09 14:58:05 +0300

    routes changed

--- app/controllers/verification_controller.rb
+++ app/controllers/verification_controller.rb
@@ -1,6 +1,7 @@
 class VerificationController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:linkedin]
   def linkedin
+    logger.warn params.permit!.to_h
     render text: params.permit!.to_h, status: :ok
   end
 end
